8aa9241aa1d1ff37f0988c2b96064f129157f2a9
YUSUF MIRZA OKSUZ <ymiirza@gmail.com>
2023-03-14 15:33:34 +0300

    eslint configuration

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ var usersRouter = require('./src/routes/users');
 var app = express();
 
 // view engine setup
+// eslint-disable-next-line no-undef
 app.set('views', path.join(__dirname, './src/views'));
 app.set('view engine', 'jade');
 
@@ -17,25 +18,26 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+// eslint-disable-next-line no-undef
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+app.use(function (req, res, next) {
+    next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+app.use(function (err, req, res, next) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
 });
 
 module.exports = app;
